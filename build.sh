52b42bd1bceb17568897cd55dc002feb07abb489
Boos4721 <43845654+Boos4721@users.noreply.github.com>
2019-07-15 15:05:56 +0800

    apt-get update

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 #
+apt-get update
+#
 sudo apt-get install -y openjdk-8-jdk git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip python bc imagemagick ccache schedtool libssl-dev libncurses* clang
 #
 mkdir -p ~/bin
